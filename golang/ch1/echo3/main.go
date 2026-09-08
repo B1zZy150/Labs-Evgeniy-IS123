1a68a0d3d7657134025992b286f604798a5bec37
B1zZy150 <zekabessonov23@gmail.com>
2026-09-08 16:33:29 +0500

    go

--- golang/ch1/echo3/main.go
+++ golang/ch1/echo3/main.go
@@ -4,18 +4,28 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println(os.Args[1:])
-	fmt.Println(strings.Join(os.Args[1:], " "))
-	fmt.Println(strings.Join(os.Args[0:], " ")) //1.1
+	fmt.Printf("  Время: %v\n", time.Since(start))
+	start = time.Now()
 
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Printf("  Время: %v\n", time.Since(start))
+	start = time.Now()
+
+	fmt.Println(strings.Join(os.Args[0:], " ")) //1.1
+	fmt.Printf("  Время: %v\n", time.Since(start))
+
+	start = time.Now()
 	var s, sep string //1.2
 	for i := 0; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 		fmt.Println(s, i)
 	}
-
+	fmt.Printf("  Время: %v\n", time.Since(start)) //1.3
 }
